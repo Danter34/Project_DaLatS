9b37e7bf3e52f495110bc27c88df54b4eea293a1
PhatPBRIT <hsthptbb.chphat11a10@gmail.com>
2026-01-20 07:52:04 +0700

    sửa giao diện login admin và users

--- SafeDalat_API/SafeDalat_API/obj/Debug/net8.0/SafeDalat_API.AssemblyInfo.cs
+++ SafeDalat_API/SafeDalat_API/obj/Debug/net8.0/SafeDalat_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeDalat_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d50c60dfffd8c4bc1bed9f5030fa16dcc60688cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8d9f7d6b47f36e81696c102391b856309ebd45")]
 [assembly: System.Reflection.AssemblyProductAttribute("SafeDalat_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeDalat_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
